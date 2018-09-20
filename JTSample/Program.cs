377a938bb67dba263a38a79da8f1f730fbaf49b6
PatrickFoley <patrick@moraware.com>
2018-09-20 12:46:15 -0400

    Add project files.

---
+++ JTSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moraware.JobTrackerAPI5;
+
+namespace JTSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var DB = "yourdatabasename";
+            var JTURL = "https://" + DB + ".moraware.net/";
+            var UID = "api";
+            // var PWD = "your password";
+
+            Connection conn = new Connection(JTURL + "api.aspx", UID, PWD);
+            conn.Connect();
+
+            int jobid = 456; // a random job in my system - yours will be different
+
+            var job = conn.GetJob(jobid);
+            Console.WriteLine("Job {0} is {1}", job.JobName, job.JobStatus);
+
+            conn.Disconnect();
+        }
+    }
+}
